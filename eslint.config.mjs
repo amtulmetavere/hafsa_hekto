dd348540669426a0dd4c1b467a5472646c259001
AmtulSara <119765009+amtulmetavere@users.noreply.github.com>
2025-01-03 21:48:01 +0500

    eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-console": "off", // Disables the "no-console" rule
+      "no-unused-vars": "warn", // Changes "no-unused-vars" to a warning
+    },
+  },
 ];
 
 export default eslintConfig;
